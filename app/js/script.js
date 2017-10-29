7db46c058e2da26af73d1f0d4aeb7ace18ab017a
Alcina <white_kaitou@hotmail.com>
2017-10-28 21:05:56 -0400

    notes for Simon Game

---
+++ app/js/script.js
@@ -0,0 +1,10 @@
+//Requirements
+
+
+//Four different coloured zones that will need to light up
+//takes user input and verifies that is it correct
+//loop through array of actions and check if correct? What is the sequence, and how long can you go?
+//each colour has a sound associated with it
+//what is strict mode?
+//unintrustive alert mode for making a mistake and playing again
+//counter to display and keep track of how many wins
